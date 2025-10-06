784e50936e574ac3b2d2fd15f135c73852a57561
yDewolf <andrersng@gmail.com>
2025-10-06 07:29:45 -0300

    Configuração inicial + Conexão com DB

---
+++ SistemaLocacaoBikes/SistemaLocacaoBikes/src/sistemalocacaobikes/dao/ConnectionFactory.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sistemalocacaobikes.dao;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+
+public class ConnectionFactory {
+    private static final String DATABASE = "locadora_bikes";
+    private static final int PORT = 3306;
+    
+    private static final String URL = "jdbc:mysql://localhost:" + PORT  + "/" + DATABASE;
+    private static final String USER = "root";
+    private static final String PASSWORD = "";
+    
+    public static Connection getConnection() throws Exception {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
